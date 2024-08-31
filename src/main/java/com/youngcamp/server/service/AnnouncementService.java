2c527d2d64e61f9b1ab752c0944d4cfaadbfa97d
Limwngur <ovg07047@naver.com>
2024-08-31 09:04:04 +0900

    feat: 공지사항 CRD기능 구현 - 공지사항 등록 - 공지사항 batch 삭제 - 공지사항 목록 조회 - Controller, Service, Repository 및 DTO 구현

--- src/main/java/com/youngcamp/server/service/AnnouncementService.java
+++ src/main/java/com/youngcamp/server/service/AnnouncementService.java
@@ -1,2 +1,71 @@
-package com.youngcamp.server.service;public class AnnouncementService {
+package com.youngcamp.server.service;
+
+import com.youngcamp.server.domain.Announcement;
+import com.youngcamp.server.dto.AnnouncementRequest.AnnouncementDeleteRequest;
+import com.youngcamp.server.dto.AnnouncementRequest.AnnouncementPostRequest;
+import com.youngcamp.server.dto.AnnouncementRequest.AnnouncementSearch;
+import com.youngcamp.server.dto.AnnouncementResponse.AnnouncementGetResponse;
+import com.youngcamp.server.dto.AnnouncementResponse.AnnouncementPostResponse;
+import com.youngcamp.server.exception.NotFoundException;
+import com.youngcamp.server.repository.AnnouncementRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class AnnouncementService {
+
+    private final AnnouncementRepository announcementRepository;
+
+    @Transactional
+    public AnnouncementPostResponse addAnnouncement(AnnouncementPostRequest request) {
+
+        Announcement announcement = Announcement.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .imageUrl(request.getImageUrl())
+                .isPinned(request.getIsPinned())
+                .build();
+
+        Announcement savedAnnouncement = announcementRepository.save(announcement);
+
+        return AnnouncementPostResponse.builder()
+                .id(savedAnnouncement.getId())
+                .build();
+    }
+
+    @Transactional
+    public void deleteAnnouncement(AnnouncementDeleteRequest request) {
+
+        existIdValidation(request.getIds());
+        announcementRepository.deleteAllAnnouncementById(request.getIds());
+    }
+
+    private void existIdValidation(List<Long> ids) {
+        List<Long> existingIds = announcementRepository.findExistingIds(ids);
+
+        List<Long> nonExistingIds = ids.stream()
+                .filter(id -> !existingIds.contains(id))
+                .collect(Collectors.toList());
+
+        if (!nonExistingIds.isEmpty()) {
+            throw new NotFoundException("Announcement", null, "존재하지 않는 ID가 포함되어 있습니다"); //TODO
+        }
+    }
+
+    public List<AnnouncementGetResponse> getAnnouncements(AnnouncementSearch announcementSearch) {
+        PageRequest pageable = PageRequest.of(announcementSearch.getPage() - 1, announcementSearch.getSize());
+
+        return announcementRepository.findAll(pageable).getContent().stream()
+                .map(AnnouncementGetResponse::new)
+                .collect(Collectors.toList());
+    }
 }
