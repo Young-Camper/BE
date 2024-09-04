952f59c3cfe5f85cadf40649279f6e5d1e08b730
Limwngur <ovg07047@naver.com>
2024-09-04 20:58:56 +0900

    fix: 공지사항 상세조회 LocalDateTime json 변환 에러 해결

--- src/main/java/com/youngcamp/server/dto/AnnouncementResponse.java
+++ src/main/java/com/youngcamp/server/dto/AnnouncementResponse.java
@@ -1,6 +1,7 @@
 package com.youngcamp.server.dto;
 
 import com.youngcamp.server.domain.Announcement;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,46 +13,45 @@ import java.time.LocalDateTime;
 @Getter
 public class AnnouncementResponse {
 
-    @Builder
-    @Getter
-    public static class AnnouncementPostResponse {
-        private Long id;
-    }
+  @Builder
+  @Getter
+  public static class AnnouncementPostResponse {
+    private Long id;
+  }
+
+  @Builder
+  @Getter
+  public static class AnnouncementEditResponse {
+    private Long id;
+  }
+
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class AnnouncementGetResponse {
+    private Long id;
+    private String title;
+    private Boolean isPinned;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     @Builder
-    @Getter
-    public static class AnnouncementEditResponse {
-        private Long id;
+    public AnnouncementGetResponse(Announcement announcement) {
+      this.id = announcement.getId();
+      this.title = announcement.getTitle();
+      this.isPinned = announcement.getIsPinned();
     }
+  }
 
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class AnnouncementGetResponse {
-        private Long id;
-        private String title;
-        private Boolean isPinned;
-        private LocalDateTime createdAt;
-        private LocalDateTime updatedAt;
-
-        @Builder
-        public AnnouncementGetResponse(Announcement announcement) {
-            this.id = announcement.getId();
-            this.title = announcement.getTitle();
-            this.isPinned = announcement.getIsPinned();
-        }
-    }
-
-    @Builder
-    @Getter
-    public static class AnnouncementGetDetailResponse {
-        private String title;
-        private String content;
-        private String imageUrl;
-        private String fileUrl;
-        private Boolean isPinned;
-        private LocalDateTime createdAt;
-        private LocalDateTime updatedAt;
-    }
-
+  @Builder
+  @Getter
+  public static class AnnouncementGetDetailResponse {
+    private String title;
+    private String content;
+    private String imageUrl;
+    private String fileUrl;
+    private Boolean isPinned;
+    private String createdAt;
+    private String updatedAt;
+  }
 }
